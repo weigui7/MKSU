d34821710812629bccc60d525b8e18adc50eb4ab
weigui404 <74092436+weigui404@users.noreply.github.com>
2025-07-15 02:10:52 +0800

    Update MainActivity.kt

--- manager/app/src/main/java/me/weigui/kernelsu/ui/MainActivity.kt
+++ manager/app/src/main/java/me/weigui/kernelsu/ui/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.displayCutout
